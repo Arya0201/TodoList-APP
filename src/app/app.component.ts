2798c3bc34c3c8bd548841fa3ee2519841df9089
Arya patel <aryapatel1119@gmail.com>
2022-05-12 11:21:44 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(){
+    setTimeout(()=>{
+      this.title="new title";
+    },2000);
+  }
 }
